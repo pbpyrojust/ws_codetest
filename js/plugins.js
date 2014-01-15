d5c77cc63c0d2ca73755073bcbab70ef108e7424
pbpyrojust <justin@justwhat.net>
2014-01-14 23:27:39 -0800

    Finished up code test

--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,3 @@
-// Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
     var noop = function () {};
@@ -14,11 +13,8 @@
     while (length--) {
         method = methods[length];
 
-        // Only stub undefined methods.
         if (!console[method]) {
             console[method] = noop;
         }
     }
 }());
-
-// Place any jQuery/helper plugins in here.
