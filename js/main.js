ab16c59d66bd3a7436bb6272c983389ec391ce52
pbpyrojust <justin@justwhat.net>
2014-01-14 22:38:44 -0800

    More css styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-
+$(document).ready(function(){
+	$('.home').click(function(){
+		$(this).addClass('active');
+		$('.about,.take').removeClass('active');
+		$('h2').text("Welcome home. I hope you enjoy your stay!");
+	});
+	$('.about').click(function(){
+		$(this).addClass('active');
+		$('.home,.take').removeClass('active');
+		$('h2').text("Welcome to about us. I hop you find what you're looking for!");
+	});
+	$('.take').click(function(){
+		$(this).addClass('active');
+		$('.home,.about').removeClass('active');
+		$('h2').text("Welcome to the WSI mark up challenge. Good luck!");
+	});
+});
